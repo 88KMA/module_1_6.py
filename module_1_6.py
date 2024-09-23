0e803146197aa2b1d091cc1e8c8d8b90fcb069ee
88KMA <maximkozlov88@gmail.com>
2024-09-23 22:38:07 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,18 +1,17 @@
 my_dict = {'Max': 1988, 'Andrey': 1969, 'Olga': 1970, 'Larisa': 1950}
-print(my_dict)
-print(my_dict['Andrey'])
-my_dict['Alena'] = 1998
-print(my_dict['Alena'])
+print('Dict:', my_dict)
+print('Existing value:', my_dict['Andrey'])
+print('Not existing value:', my_dict.get('Alena'))
 my_dict.update({'Nikolay': 1989,
                 'Yuriy': 1959})
 p_name = my_dict.pop('Olga')
-print(p_name)
-print(my_dict)
+print('Deleted value:', p_name)
+print('Modified dictionary:', my_dict)
 print('')
 
-my_set = {1, 2, 5, 4, 1, 3, 6, 4, 1, 'Game', (5, 4, 3), 'Cube', 'Game', False}
-print(my_set)
+my_set = {1, 2, 5, 4, 1, 3, 3.333, 6, 4, 1, 'Game', (5, 4, 3), 'Cube', 'Game', False}
+print('Set:', my_set)
 my_set.add(89)
 my_set.add('Python')
 my_set.remove((5, 4, 3))
-print(my_set)
+print('Modified set:', my_set)
